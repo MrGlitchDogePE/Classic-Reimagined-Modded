becbfb26d1c49476513d4dee1e7349a7fc2d6fe8
MrGlitchDogePE <hexygaming@gmail.com>
2026-01-24 21:37:39 -0600

    Improve fog calculation logic in GLSL shaders
    Refactored fog.glsl in multiple Sodium versions to enhance fog calculation logic. The changes adjust fog start and end values, improve handling of environmental and render distance fog, and clarify the distinction between overworld and special fog effects for more accurate visual results.

--- sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
@@ -2,20 +2,26 @@ const int FOG_SHAPE_SPHERICAL = 0;
 const int FOG_SHAPE_CYLINDRICAL = 1;
 
 float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
-    fogEnd *= (30.5/30); // Adjust for better visual match to original beta fog
+    fogEnd *= 30.5 / 30.0; // Adjust for better visual match to original beta fog
+    fogStart /= 4;
     if (vertexDistance <= fogStart) {
         return 0.0;
     } else if (vertexDistance >= fogEnd) {
         return 1.0;
     }
-
     return (vertexDistance - fogStart) / (fogEnd - fogStart);
 }
-
 // beta like behavior fog
-float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    return mix(linear_fog_value(sphericalVertexDistance, environmentalStart, environmantalEnd), linear_fog_value(cylindricalVertexDistance, clamp(mix(renderDistanceStart, environmentalStart, 0.75), 0, 2048), renderDistanceEnd),
-    clamp((floor(abs(environmantalEnd / 16) - 6)) + 1, 0, 1));
+float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
+    if(environmentalEnd > renderDistanceEnd) {
+        // this is probably overworld atmosphere fog, if so only use render distance fog properties
+        return max(linear_fog_value(sphericalVertexDistance, environmentalStart, environmentalEnd), linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd));
+    }
+    // otherwise use the mix of environmental and render distance
+    // in this case environmental fog would be other effects such as underwater, blindness, powder snow, etc fog
+    return max(linear_fog_value(sphericalVertexDistance,
+    environmentalStart, mix(environmentalEnd, renderDistanceEnd, clamp((floor(abs(environmentalEnd / 16) - 6)) + 1, 0, 1))), // environmental fog
+    linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd));
 }
 
 vec4 _linearFog(vec4 fragColor, vec2 fragDistance, vec4 fogColor, vec2 environmentFog, vec2 renderFog, float fadeFactor) {
@@ -28,5 +34,5 @@ vec4 _linearFog(vec4 fragColor, vec2 fragDistance, vec4 fogColor, vec2 environme
 }
 
 vec2 getFragDistance(vec3 position) {
-    return vec2(max(length(position), length(position)), length(position));
+    return vec2(length(position));
 }
