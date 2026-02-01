a8691c08229d4b785aaecabc4eb719e5dfd0e4d1
MrGlitchDogePE <211275175+MrGlitchDogePE@users.noreply.github.com>
2026-01-31 20:18:38 -0600

    Add is_nether.json for biome fog configuration

--- sodium-1.21.9/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.9/assets/sodium/shaders/include/fog.glsl
@@ -14,9 +14,8 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 }
 
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    return max(linear_fog_value(sphericalVertexDistance, environmentalStart, // environmental fog
-    mix(environmentalEnd, max(renderDistanceEnd, environmentalEnd), clamp(((abs(environmentalEnd / 16) - 6)) + 1, 0, 1))), // environmental fogEnd
-    linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd)); // render distance fog
+    return max(
+        linear_fog_value(sphericalVertexDistance, environmentalStart, environmentalEnd), linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd));
 }
 
 vec4 _linearFog(vec4 fragColor, vec2 fragDistance, vec4 fogColor, vec2 environmentFog, vec2 renderFog) {
