1a81275a7ef0748cdbd08e9af77859f1c19a7085
MrGlitchDogePE <211275175+MrGlitchDogePE@users.noreply.github.com>
2026-02-06 16:16:05 -0600

    updated fog shaders

--- sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
@@ -15,10 +15,10 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
     return max(max(max(max(max(max(
         linear_fog_value(sphericalVertexDistance, max(12, renderDistanceEnd * 0.375), renderDistanceEnd * (61 / 30.0)),
-        linear_fog_value(sphericalVertexDistance, max(32, renderDistanceEnd * 0.125), renderDistanceEnd * (45.75 / 30.0))),
-        linear_fog_value(sphericalVertexDistance, max(64, renderDistanceEnd * 0.25), renderDistanceEnd * (30.5 / 30.0))),
-        linear_fog_value(sphericalVertexDistance, renderDistanceEnd / 4, renderDistanceEnd * (30.1875 / 30.0))),
-        linear_fog_value(sphericalVertexDistance, renderDistanceStart, renderDistanceEnd)),
+        linear_fog_value(sphericalVertexDistance, max(64, renderDistanceEnd * 0.25), renderDistanceEnd * (53.375 / 30.0))),
+        linear_fog_value(sphericalVertexDistance, max(128, renderDistanceEnd * 0.5), renderDistanceEnd * (45.75 / 30.0))),
+        linear_fog_value(sphericalVertexDistance, renderDistanceEnd, renderDistanceEnd * (30.1875 / 30.0))),
+        linear_fog_value(sphericalVertexDistance, renderDistanceStart / 2, renderDistanceEnd)),
         linear_fog_value(sphericalVertexDistance, environmentalEnd / 4, environmentalEnd)),
         linear_fog_value(sphericalVertexDistance, environmentalStart, environmentalEnd))
     ;
