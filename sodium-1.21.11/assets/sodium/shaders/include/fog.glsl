5d5e62a3cb6717ede103201a75e91e250d19a54e
MrGlitchDogePE <211275175+MrGlitchDogePE@users.noreply.github.com>
2026-02-05 20:13:50 -0600

    updated fog shaders

--- sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
@@ -13,12 +13,13 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 }
 
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    return max(max(max(max(max(
+    return (max(max(max(max(max(
         linear_fog_value(sphericalVertexDistance, 0, 1024 * (30.5 / 30.0)),
         linear_fog_value(sphericalVertexDistance, 2, 768 * (30.5 / 30.0))),
         linear_fog_value(sphericalVertexDistance, 4, 512 * (30.5 / 30.0))),
         linear_fog_value(sphericalVertexDistance, renderDistanceEnd / 2, renderDistanceEnd * (30.1875 / 30.0))),
         linear_fog_value(sphericalVertexDistance, renderDistanceStart, renderDistanceEnd)),
+        linear_fog_value(sphericalVertexDistance, environmentalEnd / 4, environmentalEnd)),
         linear_fog_value(sphericalVertexDistance, environmentalStart, environmentalEnd))
     ;
 }
