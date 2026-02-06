2ee532e70858fa09ce700b216d3c59cd23453b43
MrGlitchDogePE <211275175+MrGlitchDogePE@users.noreply.github.com>
2026-02-05 20:35:48 -0600

    corrected end distance

--- sodium-1.21.6/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.6/assets/sodium/shaders/include/fog.glsl
@@ -15,8 +15,8 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
     return max(max(max(max(max(max(
         linear_fog_value(sphericalVertexDistance, 0, 1024 * (30.5 / 30.0)),
-        linear_fog_value(sphericalVertexDistance, 2, 768 * (30.5 / 30.0))),
-        linear_fog_value(sphericalVertexDistance, 4, 512 * (30.5 / 30.0))),
+        linear_fog_value(sphericalVertexDistance, 24, 768 * (30.5 / 30.0))),
+        linear_fog_value(sphericalVertexDistance, 32, 512 * (30.5 / 30.0))),
         linear_fog_value(sphericalVertexDistance, renderDistanceEnd / 2, renderDistanceEnd * (30.1875 / 30.0))),
         linear_fog_value(sphericalVertexDistance, renderDistanceStart, renderDistanceEnd)),
         linear_fog_value(sphericalVertexDistance, environmentalEnd / 4, environmentalEnd)),
--- sodium-1.21.9/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.9/assets/sodium/shaders/include/fog.glsl
@@ -15,14 +15,15 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
     return max(max(max(max(max(max(
         linear_fog_value(sphericalVertexDistance, 0, 1024 * (30.5 / 30.0)),
-        linear_fog_value(sphericalVertexDistance, 2, 768 * (30.5 / 30.0))),
-        linear_fog_value(sphericalVertexDistance, 4, 512 * (30.5 / 30.0))),
+        linear_fog_value(sphericalVertexDistance, 24, 768 * (30.5 / 30.0))),
+        linear_fog_value(sphericalVertexDistance, 32, 512 * (30.5 / 30.0))),
         linear_fog_value(sphericalVertexDistance, renderDistanceEnd / 2, renderDistanceEnd * (30.1875 / 30.0))),
         linear_fog_value(sphericalVertexDistance, renderDistanceStart, renderDistanceEnd)),
         linear_fog_value(sphericalVertexDistance, environmentalEnd / 4, environmentalEnd)),
         linear_fog_value(sphericalVertexDistance, environmentalStart, environmentalEnd))
     ;
 }
+
 vec4 _linearFog(vec4 fragColor, vec2 fragDistance, vec4 fogColor, vec2 environmentFog, vec2 renderFog) {
 #ifdef USE_FOG
     float fogValue = total_fog_value(fragDistance.y, fragDistance.x, environmentFog.x, environmentFog.y, renderFog.x, renderFog.y);
