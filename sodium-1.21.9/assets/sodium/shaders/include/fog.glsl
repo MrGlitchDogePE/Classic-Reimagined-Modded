92816ac7c0f532a4304cceb10491d9a483000b98
MrGlitchDogePE <211275175+MrGlitchDogePE@users.noreply.github.com>
2026-02-07 12:07:02 -0600

    fixed spelling in fog shaders

--- sodium-1.21.9/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.9/assets/sodium/shaders/include/fog.glsl
@@ -12,8 +12,8 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
     return (vertexDistance - fogStart) / (fogEnd - fogStart);
 }
 
-float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    if(environmantalEnd > renderDistanceEnd) {
+float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
+    if(environmentalEnd > renderDistanceEnd) {
         // this is probably overworld atmosphere fog, if so only use render distance fog properties
         return mix(
         linear_fog_value(sphericalVertexDistance, renderDistanceEnd, renderDistanceEnd),
@@ -23,8 +23,8 @@ float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDist
     // otherwise use the mix of environmental and render distance
     // in this case environmental fog would be other effects such as underwater, blindness, powder snow, etc fog
     return max(
-        linear_fog_value(sphericalVertexDistance, environmentalStart, environmantalEnd),
-        linear_fog_value(sphericalVertexDistance, environmentalStart / 4, environmantalEnd));
+        linear_fog_value(sphericalVertexDistance, environmentalStart, environmentalEnd),
+        linear_fog_value(sphericalVertexDistance, environmentalStart / 4, environmentalEnd));
 }
 
 vec4 _linearFog(vec4 fragColor, vec2 fragDistance, vec4 fogColor, vec2 environmentFog, vec2 renderFog) {
