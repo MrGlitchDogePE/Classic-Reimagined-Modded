e7ad6241f7afc67372f4906227c216818f3f0e0d
MrGlitchDogePE <211275175+MrGlitchDogePE@users.noreply.github.com>
2026-03-14 16:44:04 -0600

    Refactor total_fog_value function for enhanced fog calculations and performance

--- sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
@@ -13,11 +13,11 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 }
 
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    return mix(
-        linear_fog_value(sphericalVertexDistance, environmentalStart, environmentalEnd),
-        pow(linear_fog_value(sphericalVertexDistance, 0, 1024), (1.0f - linear_fog_value(sphericalVertexDistance, 0, min(renderDistanceEnd, environmentalEnd))) * 0.75),
-        clamp(pow(linear_fog_value(sphericalVertexDistance, 0, min(renderDistanceEnd, environmentalEnd)),
-        2.75 * (1.0f - linear_fog_value(sphericalVertexDistance, 0, min(renderDistanceEnd, environmentalEnd)))), 0.0, 1.0)
+    return max(
+        linear_fog_value(cylindricalVertexDistance, environmentalStart * 2, environmentalEnd * 2),
+        pow(linear_fog_value(sphericalVertexDistance, 0, 1024),
+        (1.0f - linear_fog_value(sphericalVertexDistance, min(8, min(renderDistanceEnd, environmentalEnd) / 4), min(renderDistanceEnd, environmentalEnd))) * 1.25 *
+        (min(8, min(renderDistanceEnd, environmentalEnd) / 4) / 8))
     );
 }
 
