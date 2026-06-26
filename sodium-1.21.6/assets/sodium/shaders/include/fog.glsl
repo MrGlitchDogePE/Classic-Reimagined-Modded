6dd6f8309684eea4c025ac81db006658544af363
DogeLinux <hexygaming99@gmail.com>
2026-06-25 22:35:39 -0600

    updated fog shaders

--- sodium-1.21.6/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.6/assets/sodium/shaders/include/fog.glsl
@@ -13,7 +13,9 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
     return mix(
-        pow(clamp((sphericalVertexDistance) / min(renderDistanceEnd, environmentalEnd), 0.0, 1.0), 2),
+        mix(pow(clamp((sphericalVertexDistance) / min(renderDistanceEnd, environmentalEnd), 0.0, 1.0), 2.5),
+        1.0,
+        pow(clamp((sphericalVertexDistance) / 1024, 0.0, 1.0), 1.38)),
         1.0,
         clamp((0.5 - environmentalStart) / (environmentalEnd - environmentalStart), 0.0, 1.0)
     );
