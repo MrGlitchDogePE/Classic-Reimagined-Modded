5e0a19fa56f1ee58627977d5d7d94316bc20bb60
DogeLinux <hexygaming99@gmail.com>
2026-09-23 19:35:37 -0600

    updated fog shaders

--- sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
+++ sodium-1.21.11/assets/sodium/shaders/include/fog.glsl
@@ -14,20 +14,20 @@ float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
 float classic_fog_value(float vertexDistance, float fogStart, float fogEnd) {
         float denom = fogEnd - fogStart;
         float fogFactor = clamp((fogEnd - vertexDistance) / (denom + 0.001), 0.0, 1.0);
-    return 1.0f - fogFactor; 
+    return mix(1.0, 0.0, fogFactor); 
 }
 
 float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmentalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    float classicEnd = min(environmentalEnd, renderDistanceEnd);
+    float classicEnd = min(renderDistanceEnd, environmentalEnd);
     float classicStart = classicEnd * 0.25;
-    if(environmentalEnd == 96.0 && environmentalStart == 10.0) { // if isNether = true returns the classic nether fog
-        classicEnd = renderDistanceEnd;
+    if (environmentalStart == 10.0 && environmentalEnd == 96.0) { // classic nether fog, use render distance fog properties
+        classicEnd = renderDistanceStart;
+        classicStart = 0.0;
     }
-    return mix(
-        classic_fog_value(sphericalVertexDistance, classicStart, classicEnd),
-        1.0,
-        clamp((0.0 - environmentalStart) / (environmentalEnd - environmentalStart), 0.0, 1.0)
-    );
+    if (environmentalStart == -8.0 && environmentalEnd <= 96.0) { // water fog, use render distance fog properties
+        classicStart = min(classicEnd * 0.25, (environmentalStart / environmentalEnd) * classicEnd);
+    }
+    return classic_fog_value(sphericalVertexDistance, classicStart, classicEnd);
 }
 
 vec4 _linearFog(vec4 fragColor, vec2 fragDistance, vec4 fogColor, vec2 environmentFog, vec2 renderFog, float fadeFactor) {
